d9a0fbc9c0776dba24a34e145beb3d0cf3f133ed
minglie <934031452@qq.com>
2021-07-05 16:49:31 +0800

    aa

--- githubWebhookForMac.js
+++ githubWebhookForMac.js
@@ -5,9 +5,9 @@ app.listen(8888)
 
 app.post("langjiedoc/push",async (req,res)=>{
     console.log(req.url)
-    console.log("文档被修改,重新部署中")
+    console.log(new Date()+ " 文档被修改,重新部署中")
     let r=await M.exec("git pull")
-    console.log("已重新部署")
+    console.log(new Date()+ "已重新部署")
     console.log(r)
     res.send("ok")
 })
