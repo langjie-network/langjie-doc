64f47622a3de5814594806a1a4e3df8124d71c49
minglie <934031452@qq.com>
2021-07-05 16:43:56 +0800

    aa

---
+++ githubWebhookForMac.js
@@ -0,0 +1,24 @@
+M=require("ming_node")
+
+app=M.server()
+app.listen(8888)
+
+app.post("langjiedoc/push",async (req,res)=>{
+    console.log(req.url)
+    console.log("文档被修改,重新部署中")
+    let r=await M.exec("git pull")
+    console.log("已重新部署")
+    console.log(r)
+    res.send("ok")
+})
+
+
+app.get("test",async (req,res)=>{
+    console.log(req.url)
+    console.log("文档被修改")
+    let r=await M.exec("git pull")
+    console.log("已重新部署")
+    console.log(r)
+    res.send(r)
+})
+
